6248f5fc61eead9690ff7b0b90c9ac5b99fc506b
Mussab <mussabna9@gmail.com>
2025-11-11 01:16:14 +0500

    Fix language setting: Change from English to Italian
    - Changed language_config from "You need to speak English" to "You need to speak Italian"
    - Fixes accidental English language in production deployment

--- config/settings.py
+++ config/settings.py
@@ -113,7 +113,7 @@ class Settings:
     @property
     def language_config(self) -> str:
         """Global language instruction for prompts"""
-        return "You need to speak English"
+        return "You need to speak Italian"
 
     @property
     def llm_interpretation_config(self) -> Dict[str, Any]:
