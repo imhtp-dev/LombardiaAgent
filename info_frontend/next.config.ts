9a16ff60d1df5d6a4b2a60fbd0a22da46586da08
Rahees Ahmed <raheesahmed256@gmail.com>
2025-11-13 07:24:00 +0500

    feat: add standalone output configuration for Docker in Next.js

--- info_frontend/next.config.ts
+++ info_frontend/next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  /* config options here */
+  output: 'standalone', // Enable standalone build for Docker
   typescript:{
     ignoreBuildErrors:true
   },
